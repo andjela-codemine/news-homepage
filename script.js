7fae13643c3cde1f4086b5c34f045ba3bf96591d
Andjela Vucic <andjela.vucic@codemine.me>
2024-02-19 10:46:39 +0100

    COD-100: refactor: mobile menu toggle functionality

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const btn = document.querySelector(".mobile-menu-button");
-const nav = document.querySelector("nav");
-const body = document.querySelector("body");
-const menuIcon = document.querySelector(".mobile");
-const overlay = document.querySelector(".overlay");
+const menuBtn = document.querySelector('.mobile-menu-button');
+const nav = document.querySelector('nav');
+const body = document.querySelector('body');
+const menuIcon = document.querySelector('.mobile-menu-icon');
+const overlay = document.querySelector('.overlay');
 
-btn.addEventListener("click", () => {
-  nav.classList.toggle("show");
-  overlay.style.display = nav.classList.contains("show") ? "block" : "none";
-  overlay.style.opacity = nav.classList.contains("show") ? 1 : 0;
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('show');
+    overlay.style.display = nav.classList.contains('show') ? 'block' : 'none';
+    overlay.style.opacity = nav.classList.contains('show') ? '1' : '0';
 
-  if (menuIcon.src.includes("icon-menu.svg")) {
-    menuIcon.src = "./assets/images/icon-menu-close.svg";
-    body.style.overflow = "hidden"; //disable scrolling
-  } else {
-    menuIcon.src = "./assets/images/icon-menu.svg";
-    body.style.overflow = "auto";
-  }
+    if (menuIcon.src.includes('icon-menu.svg')) {
+        menuIcon.src = './assets/images/icon-menu-close.svg';
+        body.style.overflow = 'hidden';
+    } else {
+        menuIcon.src = './assets/images/icon-menu.svg';
+        body.style.overflow = 'auto';
+    }
 });
